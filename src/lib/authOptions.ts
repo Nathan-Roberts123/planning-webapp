5cf311c18bb6998de7183e67cfeda79bcd8e059a
Pieter Stols <robertsnathan2004@gmail.com>
2024-06-04 22:57:58 +0200

    small commit

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -36,7 +36,7 @@ export const authOptions: AuthOptions = {
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+        const user = { id: "2", name: "J Smith", email: "jsmith@example.com" };
 
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
